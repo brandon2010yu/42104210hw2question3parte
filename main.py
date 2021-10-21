6fc31bac8785553183332e43a31aa138631d5807
brandonyu <brandon2010yu@gmail.com>
2021-10-20 18:47:04 -0700

    updated

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from sklearn.neighbors import KNeighborsClassifier
 import csv
 
 db = []
+number_of_pred = 0
+wrong_predict = 0
 
 # reading the data in a csv file
 with open('binary_points.csv', 'r') as csvfile:
@@ -26,83 +28,54 @@ for i, instance in enumerate(db):
     # add the training features to the 2D array X removing the instance that will be used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]]. Convert each feature value to
     # float to avoid warning messages
     # --> add your Python code here
-    X = [[0, 3], [1, 4], [2, 4], [3, 3], [2, 1], [4, 4], [4, 3], [3, 2], [4, 1]]
-    X2 = [[0, 3], [1, 4], [2, 4], [3, 3], [2, 1], [4, 4], [4, 3], [3, 2], [0, 5]]
-    X3 = [[0, 3], [1, 4], [2, 4], [3, 3], [2, 1], [4, 4], [4, 3], [4, 1], [0, 5]]
-    X4 = [[0, 3], [1, 4], [2, 4], [3, 3], [2, 1], [4, 4], [3, 2], [4, 1], [0, 5]]
-    X5 = [[0, 3], [1, 4], [2, 4], [3, 3], [2, 1], [4, 3], [3, 2], [4, 1], [0, 5]]
-    X6 = [[0, 3], [1, 4], [2, 4], [3, 3], [4, 4], [4, 3], [3, 2], [4, 1], [0, 5]]
-    X7 = [[0, 3], [1, 4], [2, 4], [2, 1], [4, 4], [4, 3], [3, 2], [4, 1], [0, 5]]
-    X8 = [[0, 3], [1, 4], [3, 3], [2, 1], [4, 4], [4, 3], [3, 2], [4, 1], [0, 5]]
-    X9 = [[0, 3], [2, 4], [3, 3], [2, 1], [4, 4], [4, 3], [3, 2], [4, 1], [0, 5]]
-    X10 = [[1, 4], [2, 4], [3, 3], [2, 1], [4, 4], [4, 3], [3, 2], [4, 1], [0, 5]]
+    X = []
+    for sample in db:
+        if sample != instance:
+            X.append([float(sample[0]), float(sample[1])])
+
+
+
 
     # transform the original training classes to numbers and add to the vector Y removing the instance that will be used for testing in this iteration. For instance, Y = [1, 2, ,...]. Convert each
     #  feature value to float to avoid warning messages
     # --> add your Python code here
-    Minus = 1
-    Add = 2
-    Y = [1, 1, 2, 2, 1, 2, 2, 2, 1]
-    Y2 = [1, 1, 2, 2, 1, 2, 2, 2, 1]
-    Y3 = [1, 1, 2, 2, 1, 2, 2, 1, 1]
-    Y4 = [1, 1, 2, 2, 1, 2, 2, 1, 1]
-    Y5 = [1, 1, 2, 2, 1, 2, 2, 1, 1]
-    Y6 = [1, 1, 2, 2, 2, 2, 2, 1, 1]
-    Y7 = [1, 1, 2, 1, 2, 2, 2, 1, 1]
-    Y8 = [1, 1, 2, 1, 2, 2, 2, 1, 1]
-    Y9 = [1, 2, 2, 1, 2, 2, 2, 1, 1]
-    Y10 = [1, 2, 2, 1, 2, 2, 2, 1, 1]
+    Y = []
+    for sample in db:
+        if sample != instance:
+            if sample[2] == '+':
+                Y.append(1)
+            else:
+                Y.append(0)
+
 
     # store the test sample of this iteration in the vector testSample
     # --> add your Python code here
-    testSample = [0, 5]
-    testSample2 = [4, 1]
-    testSample3 = [3, 2]
-    testSample4 = [4, 3]
-    testSample5 = [4, 4]
-    testSample6 = [2, 1]
-    testSample7 = [3, 3]
-    testSample8 = [2, 4]
-    testSample9 = [1, 4]
-    testSample10 = [0, 3]
+    testSample = []
+    testSample.append([float(instance[0]),float(instance[1])])
 
     # fitting the knn to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
-    clf = clf.fit(X9, Y9)
+    clf = clf.fit(X, Y)
 
     # use your test sample in this iteration to make the class prediction. For instance:
     # class_predicted = clf.predict([[1, 2]])[0]
     # --> add your Python code here
-    class_predicted = clf.predict([[0, 5]])[0]
-    class_predicted2 = clf.predict([[4, 1]])[0]
-    class_predicted3 = clf.predict([[3, 2]])[0]
-    class_predicted4 = clf.predict([[4, 3]])[0]
-    class_predicted5 = clf.predict([[4, 4]])[0]
-    class_predicted6 = clf.predict([[2, 1]])[0]
-    class_predicted7 = clf.predict([[3, 3]])[0]
-    class_predicted8 = clf.predict([[2, 4]])[0]
-    class_predicted9 = clf.predict([[1, 4]])[0]
-    class_predicted10 = clf.predict([[0, 3]])[0]
+
+    class_predicted = clf.predict(testSample)[0]
+
+    number_of_pred += 1
 
     # compare the prediction with the true label of the test instance to start calculating the error rate.
     # --> add your Python code here
+    if class_predicted == 0 and instance[2] == '+' or class_predicted == 1 and instance[2] == '-':
+        wrong_predict += 1
 
-    # 0 class_predicted = clf.predict([[0, 5]])[0]
-    # 1 class_predicted2 = clf.predict([[4, 1]])[0] incorrect prediction
-    # 2 class_predicted3 = clf.predict([[3, 2]])[0]
-    # 3 class_predicted4 = clf.predict([[4, 3]])[0]
-    # 4 class_predicted5 = clf.predict([[4, 4]])[0]
-    # 5 class_predicted6 = clf.predict([[2, 1]])[0] incorrect prediction
-    # 6 class_predicted7 = clf.predict([[3, 3]])[0]
-    # 7 class_predicted8 = clf.predict([[2, 4]])[0] incorrect prediction
-    # 8 class_predicted9 = clf.predict([[1, 4]])[0] incorrect prediction
-    # 9 class_predicted10 = clf.predict([[0, 3]])[0]
 
-if class_predicted9 == 1 and db[6][2] != "-" or class_predicted9 == 2 and db[6][2] != "+":
-    print("wrong prediction instance")
+
+
 
 # print the error rate
 # --> add your Python code here
-wrongPrediction = 4
-errorRate = wrongPrediction / 10
+errorRate = wrong_predict / number_of_pred
 print(errorRate)
+
